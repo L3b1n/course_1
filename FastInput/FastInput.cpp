6601b233bfffea674e62fb10014838a39dd29ffd
L3b1n <binleo07@icloud.com>
2022-12-12 14:07:55 +0300

    Add functions of fast input output

--- FastInput/FastInput.cpp
+++ FastInput/FastInput.cpp
@@ -35,6 +35,26 @@ int getInt()
     return sign * answ;
 }
 
+template<typename T>
+inline void read(T& v) 
+{
+    char c = '?';
+    while(!(c == '-' || ('0' <= c && c <= '9'))){ c = getChar();}
+    bool positive = true;
+    if(c == '-') 
+    {
+        positive = false;
+        c = getChar();
+    }
+    v = 0;
+    while('0' <= c && c <= '9') 
+    {
+        (v *= 10) += (c - '0');
+        c = getChar();
+    }
+    if(!positive){ v = -v;}
+}
+
 void putChar(char c) 
 {
     static char buffer[1024*1024];
@@ -47,6 +67,13 @@ void putChar(char c)
     if(c != -1){ buffer[size++] = c;}
 }
 
+inline void writeln(int v) 
+{
+    char buf[15];
+    sprintf(buf, "%d\n", v);
+    for(char* cur = buf; *cur != '\0'; ++cur){ putChar(*cur);}
+}
+
 int main()
 {
     std::ios_base::sync_with_stdio(0);
@@ -54,7 +81,14 @@ int main()
     // freopen("out.txt", "w", stdout);
     std::cin.tie(0);
     std::cout.tie(0);
-    int test = getInt();
-    std::cout << test << "\n";
+    std::cout << "Enter integer number:\n";
+    int test1 = getInt();
+    std::cout << "Enter float number: ";
+    double test2; read(test2);
+    std::cout << "Tested getInt:\n" << test1 << "\n";
+    std::cout << "Tested read:\n" << test2 << "\n";
+    std::cout << "Tested writeln:\n";
+    writeln(test1);
+    putChar(-1);
     return 0;
 }
