64393e2fd36aa76f7c1fd5af5c50821dfcb02a60
L3b1n <binleo07@icloud.com>
2023-03-03 13:01:31 +0300

    Add swift file StoryView int folder Test swift

---
+++ Test swift/StoryView.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+
+struct StoryView: View {
+    var body: some View {
+        VStack {
+            Text("My Story")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+            
+            ScrollView {
+                Text(information.story)
+                    .offset(y: 600)
+                    .font(.body)
+                    .padding()
+            }
+        }
+        .padding([.top, .bottom], 50)
+    }
+}
+
+struct StoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        StoryView()
+    }
+}
