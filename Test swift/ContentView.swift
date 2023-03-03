6eb5077c7c7a04cb253876b73db7429206c26f02
L3b1n <binleo07@icloud.com>
2023-03-03 12:11:55 +0300

    Add swift file ContentView in folder Test swift

---
+++ Test swift/ContentView.swift
@@ -0,0 +1,220 @@
+import SwiftUI
+import Foundation
+
+struct Info {
+    let image: String
+    let name: String
+    let story: String
+    let hobbies: [String]
+    let foods: [String]
+    let colors: [Color]
+    let funFacts: [String]
+}
+
+let information = Info(
+    image: "Placeholder",
+    name: "My Name",
+    story: "A story can be about anything you can dream up. There are no right answers, there is no one else.\n\nNeed some inspiration?\n• 🐶🐱🛶️🎭🎤🎧🎸\n• 🏄‍♀️🚵‍♀️🚴‍♀️⛵️🥾🏂⛷📚\n• ✍️🥖☕️🏋️‍♂️🚲🧗‍♀️ ",
+    hobbies: ["bicycle", "ticket.fill", "book.fill"],
+    foods: ["🥐", "🌮", "🍣"],
+    colors: [Color.blue, Color.purple, Color.pink],
+    funFacts: [
+        "The femur is the longest and largest bone in the human body.",
+        "The moon is 238,900 miles away.",
+        "Prince’s last name was Nelson.",
+        "503 new species were discovered in 2020.",
+        "Ice is 9 percent less dense than liquid water.",
+        "You can spell every number up to 1,000 without using the letter A.\n\n...one, two, three, four...ninety-nine...nine hundred ninety-nine 🧐",
+        "A collection of hippos is called a bloat.",
+        "White sand beaches are made of parrotfish poop.",
+    ]
+)
+
+//struct ContentView: View {
+//    var body: some View {
+//        ScrollView() {
+//            VStack {
+//                MapView()
+//                    .ignoresSafeArea(edges: .top)
+//                    .frame(height: 300)
+//
+//                CircleImage()
+//                    .offset(y: -260)
+//                    .padding(.bottom, -400)
+//
+//
+//
+//                VStack(alignment: .leading) {
+//                    Text("Turtle Rock")
+//                        .font(.title)
+//                    HStack {
+//                        Text("Joshua Tree National Park")
+//                            .font(.subheadline)
+//                        Spacer()
+//                        Text("California")
+//                            .font(.subheadline)
+//                    }
+//
+//                    Divider()
+//                    HStack {
+//                        Text("This is the most famous place in the California")
+//                            .font(.title2)
+//                    }
+//                }
+//                .padding()
+//
+//                Spacer()
+//
+//                Text("Next page")
+//                    .foregroundColor(Color.red)
+//                    .font(.largeTitle)
+//                    .offset(y: 300)
+//            }
+//        }
+//    }
+//}
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "person")
+                }
+
+            StoryView()
+                .tabItem {
+                    Label("Story", systemImage: "book")
+                }
+            
+            FavoritesView()
+                .tabItem {
+                    Label("Favorites", systemImage: "star")
+                }
+            
+            FunFactsView()
+                .tabItem {
+                    Label("Fun Facts", systemImage: "hand.thumbsup")
+                }
+        }
+        
+    }
+}
+
+struct HomeView: View {
+    
+    var body: some View {
+        VStack {
+            Text("All About")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+
+            Image(information.image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(10)
+                .padding(40)
+
+            Text(information.name)
+                .font(.title)
+        }
+    }
+    
+}
+
+struct StoryView: View {
+    var body: some View {
+        VStack {
+            Text("My Story")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+
+            ScrollView {
+                Text(information.story)
+                    //.offset(y: 600)
+                    .font(.body)
+                    .padding()
+            }
+        }
+        .padding([.top, .bottom], 50)
+    }
+}
+
+struct FavoritesView: View {
+    var body: some View {
+        VStack {
+            Text("Favorites")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(.bottom, 40)
+            
+            Text("Hobbies")
+                .font(.title2)
+            
+            HStack {
+                ForEach(information.hobbies, id: \.self) { hobby in
+                    Image(systemName: hobby)
+                        .resizable()
+                        .frame(maxWidth: 80, maxHeight: 60)
+                    
+                }
+                .padding()
+            }
+            .padding()
+
+            Text("Foods")
+                .font(.title2)
+            
+            HStack(spacing: 60) {
+                ForEach(information.foods, id: \.self) { food in
+                    Text(food)
+                        .font(.system(size: 48))
+                }
+            }
+            .padding()
+
+            Text("Favorite Colors")
+                .font(.title2)
+
+            HStack(spacing: 30) {
+                ForEach(information.colors, id: \.self) { color in
+                    color
+                        .frame(width: 70, height: 70)
+                        .cornerRadius(10)
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+struct FunFactsView: View {
+
+    @State private var funFact = ""
+    var body: some View {
+        VStack {
+            Text("Fun Facts")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                        
+            Text(funFact)
+                .padding()
+                .font(.title)
+                .frame(minHeight: 400)
+
+            Button("Show Random Fact") {
+                funFact = information.funFacts.randomElement()!
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice("iPhone 11 Pro Max")
+    }
+}
